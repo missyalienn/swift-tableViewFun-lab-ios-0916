814a181c410545d7057c05fe3b8c63de8b307db1
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-10-06 13:55:16 -0400

    cats meow meow meow

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,41 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    let catBreeds = [
     
+    "Abyssinian",
+    "American Shorthair",
+    "Bengal",
+    "Birman",
+    "British Shorthair",
+    "Burmese",
+    "Exotic Shorthair",
+    "Maine Coon",
+    "Norwegian Forest",
+    "Persian",
+    "Ragdoll",
+    "Tabby"            ]
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catBreeds.count
+    }
+    
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catTitle = catBreeds[indexPath.row]
+        cell.textLabel?.text = catTitle
+        return cell
+    }
    
+    
+    
     
     
 }
